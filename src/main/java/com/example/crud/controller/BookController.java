8da5a15041f6a3c28cfa359579490f85c903c724
Chamoth Ketipearachi <cketipearachchi@gmail.com>
2023-10-29 22:51:28 +0530

    Add Commnets to the BookController.java Class

--- src/main/java/com/example/crud/controller/BookController.java
+++ src/main/java/com/example/crud/controller/BookController.java
@@ -12,26 +12,30 @@ import java.util.List;
 public class BookController {
 
     @Autowired
-    private BookService bookService;
+    private BookService bookService; // Inject the BookService to handle business logic.
 
+    // Inject the BookService to handle business logic.
     @RequestMapping("/books")
     public List<Book> getAllBooks(){
         return bookService.getAllBooks();
 
     }
 
+    // Endpoint for adding a new book
     @RequestMapping(method = RequestMethod.POST, value = "/books")
     public void addBook(@RequestBody Book book){
         bookService.addBook(book);
 
     }
 
+    // Endpoint for updating an existing book by ID
     @RequestMapping(method = RequestMethod.PUT, value = "/books/{id}")
     public void updateBook(@PathVariable String id, @RequestBody Book book){
         bookService.updateBook(id, book);
 
     }
 
+    // Endpoint for deleting a book by ID
     @RequestMapping(method = RequestMethod.DELETE, value = "/books/{id}")
     public void deleteBook(@PathVariable String id){
         bookService.deleteBook(id);
