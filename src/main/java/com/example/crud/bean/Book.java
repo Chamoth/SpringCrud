9955e1865e74f974583d468302b75059e69cfc34
Chamoth Ketipearachi <cketipearachchi@gmail.com>
2023-10-29 22:56:22 +0530

    Add Cooments to the Book.java Class

--- src/main/java/com/example/crud/bean/Book.java
+++ src/main/java/com/example/crud/bean/Book.java
@@ -1,6 +1,5 @@
 package com.example.crud.bean;
 
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -10,29 +9,34 @@ import javax.persistence.Table;
 public class Book {
 
     @Id
-    private String id;
-    private String name;
+    private String id;  // Unique identifier for the book.
+    private String name;  // The name or title of the book.
 
     public Book() {
+        // Default constructor.
     }
 
     public Book(String id, String name) {
-        this.id = id;
-        this.name = name;
+        this.id = id;  // Initialize the book's ID.
+        this.name = name;  // Initialize the book's name.
     }
 
+    // Getter for the book's ID.
     public String getId(){
         return id;
     }
 
+    // Setter for the book's ID.
     public void setId(String id) {
         this.id = id;
     }
 
+    // Getter for the book's name.
     public String getName() {
         return name;
     }
 
+    // Setter for the book's name.
     public void setName(String name) {
         this.name = name;
     }
